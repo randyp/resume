ee7a9805a91a7a73dd8c8df9a1418f3cd7b629c8
Randy Pensinger <randy@mediaalpha.com>
2022-02-21 14:09:21 -0800

    simplify to index

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -e
 MARKDOWN_VERSION="1.0.1"
 
 rm -f resume.html
-cat fragments/start.frag.html > resume.html
-perl "Markdown_${MARKDOWN_VERSION}"/Markdown.pl README.md >> resume.html
-cat fragments/end.frag.html >> resume.html
+cat fragments/start.frag.html > index.html
+perl "Markdown_${MARKDOWN_VERSION}"/Markdown.pl README.md >> index.html
+cat fragments/end.frag.html >> index.html
