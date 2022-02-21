eed9f571964b04a5d64e67a170a2f9c111f9a484
Randy Pensinger <randy@mediaalpha.com>
2022-02-21 14:12:38 -0800

    to the docs directory

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -e
 MARKDOWN_VERSION="1.0.1"
 
 rm -f resume.html
-cat fragments/start.frag.html > index.html
-perl "Markdown_${MARKDOWN_VERSION}"/Markdown.pl README.md >> index.html
-cat fragments/end.frag.html >> index.html
+cat fragments/start.frag.html > docs/index.html
+perl "Markdown_${MARKDOWN_VERSION}"/Markdown.pl README.md >> docs/index.html
+cat fragments/end.frag.html >> docs/index.html
